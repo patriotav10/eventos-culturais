a451bb53822cc89f3cd3824e2b4468b3e180d103
netop <jcpn1@discente.ifpe.edu.br>
2025-03-25 20:33:17 -0300

    commit inicial

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get("/", function(req, res){
+    res.send("pagina inicial");
+});
+
+app.listen("999", function(){
+    console.log("Rodando");
+});
+
