0917b818e685dc4b2cbfc11ec2046e67f2069470
netop <jcpn1@discente.ifpe.edu.br>
2025-03-25 21:49:57 -0300

    versao1

---
+++ src/aluno.js
@@ -0,0 +1,10 @@
+class Aluno{
+    constructor(nome, matricula,curso) {
+    this.nome = nome;
+    this.matricula = matricula;
+    this.curso = curso;
+    }
+};
+
+module.exports = Aluno;
+
--- src/server.js
+++ src/server.js
@@ -1,5 +1,27 @@
+
 const express = require('express');
 const app = express();
+
+const Aluno = require("./aluno");
+const aluno1 = new Aluno("Rafael", "ifpe32","informatica");
+const aluno2 = new Aluno("ze", "ifpe44","informatica");
+const aluno3 = new Aluno("Rafael", "ifpe55","informatica");
+const alunoV = [aluno1,aluno2,aluno3];
+
+app.get("/alunos", function(req, res){
+    let html = "";
+    for (const aluno of alunoV) {
+        html += 
+        <b>Nome:</b> ${aluno.nome} <br>
+        <b>matricula:</b> ${aluno.matricula} <br>
+        <b>Curso:</b> ${aluno.curso} <br>
+        <hr>
+        ;
+        
+    }
+    res.send(html);
+});
+
 
 app.get("/", function(req, res){
     res.send("pagina inicial");
